e5f228aa93d5d267713abc34db80122d1922af6a
Anna Goloshchapova <annagol.work@mail.ru>
2022-08-03 19:06:57 +0300

    Initial Commit

---
+++ R - news/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  R - news
+//
+//  Created by anna on 03.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

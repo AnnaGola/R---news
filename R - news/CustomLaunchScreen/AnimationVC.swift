465381452494dec5a9b24178cf55e3ab4ad69839
Anna Goloshchapova <annagol.work@mail.ru>
2022-08-03 21:11:14 +0300

    custom launch screen animation with lottie json

---
+++ R - news/CustomLaunchScreen/AnimationVC.swift
@@ -0,0 +1,54 @@
+//
+//  AnimationVC.swift
+//  R - news
+//
+//  Created by anna on 03.08.2022.
+//
+
+import UIKit
+import Lottie
+
+class AnimationVC: UIViewController {
+
+    var timer = Timer()
+    
+    let viewAnimation: AnimationView = {
+        let viewAnimation = AnimationView(name: "news")
+        viewAnimation.animationSpeed = 1.5
+        return viewAnimation
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: false, block: { timer in
+            self.animateView(self.viewAnimation)
+        })
+        
+        viewAnimation.contentMode = .scaleAspectFit
+        viewAnimation.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(viewAnimation)
+        viewAnimation.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        viewAnimation.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        viewAnimation.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        viewAnimation.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        
+        viewAnimation.play()
+    }
+    
+    func animateView(_ viewAnimate: AnimationView) {
+        
+        UIView.animate(withDuration: 0.4, delay: 0, options: .curveEaseIn) {
+            viewAnimate.alpha = 0
+        } completion: { done in
+            if done {
+                DispatchQueue.main.asyncAfter(deadline: .now()+0.3) {
+                     let vc = ViewController()
+                    vc.modalTransitionStyle = .crossDissolve
+                    vc.modalPresentationStyle = .fullScreen
+                    self.present(vc, animated: true)
+                }
+            }
+        }
+    }
+}

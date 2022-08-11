62a9b608638e5c0244e5395de8fdc7926ff2ae9b
Anna Goloshchapova <annagol.work@mail.ru>
2022-08-11 20:48:54 +0300

    hello world

--- R - news/Controllers/LogInVC.swift
+++ R - news/Controllers/LogInVC.swift
@@ -17,6 +17,7 @@ class LogInVC: UIViewController {
 
         view.backgroundColor = #colorLiteral(red: 0.9724035859, green: 0.9314741492, blue: 0.9023552537, alpha: 1)
         setupLogIn()
+        print("Hello world!")
     }
     
     private func setupLogIn() {

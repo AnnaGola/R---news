b201260a1f31153d37df27005c314e4b89655565
Anna Goloshchapova <annagol.work@mail.ru>
2022-08-19 22:55:42 +0300

    tableView extension for smooth animation

---
+++ R - news/Extensions/TableView+.swift
@@ -0,0 +1,35 @@
+//
+//  TableView+.swift
+//  R - news
+//
+//  Created by anna on 19.08.2022.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func animateTableView() {
+        self.reloadData()
+        
+        let cells = self.visibleCells
+        let tableViewHight = self.bounds.height
+        
+        var delayOfAnimation: Double = 0
+        
+        for cell in cells {
+            cell.transform = CGAffineTransform(translationX: 0, y: tableViewHight)
+            
+            UIView.animate(
+                withDuration: 1,
+                delay: delay * 0.1,
+                usingSpringWithDamping: 0.8,
+                initialSpringVelocity: 0,
+                options: .curveEaseInOut) {
+                    cell.transform = CGAffineTransform.identity
+                }
+            
+            delayOfAnimation += 1
+        }
+    }
+}

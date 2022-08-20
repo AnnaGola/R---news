ace6016a69e7f5ffdf1e909345140ebcc144a592
Anna Goloshchapova <annagol.work@mail.ru>
2022-08-20 13:12:14 +0300

    setup base cell

--- R - news/News/View/Cells/BaseCell.swift
+++ R - news/News/View/Cells/BaseCell.swift
@@ -9,85 +9,132 @@ import UIKit
 import SnapKit
 import Kingfisher
 
-final class HeaderNewsTableViewCell: UITableViewCell {
-    
-    static let identifier = "HeaderNewsTableViewCell"
+final class BaseCell: UITableViewCell, Loadable {
 
-    private let iconImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.backgroundColor = .secondarySystemBackground
-        imageView.contentMode = .scaleAspectFill
-        imageView.layer.cornerRadius = 10
-        imageView.clipsToBounds = true
-        return imageView
-    }()
+//MARK: - Identifier
+    static let identifier = "BaseCell"
+
+//MARK: - Properties
+    private var iconImageLayer = CAGradientLayer()
+    private var titleLayer = CAGradientLayer()
+    private var dateLayer = CAGradientLayer()
+    private var publisherLayer = CAGradientLayer()
+    private let iconImageView = UIImageView()
+    private let titleLabel = UILabel()
+    private let dateLabel = UILabel()
+    private let publisherLabel = UILabel()
     
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 2
-        label.textColor = .label
-        label.textAlignment = .left
-        label.font = .systemFont(ofSize: 18, weight: .medium)
-        return label
-    }()
-    
-    private let dateLabel: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 1
-        label.textColor = .secondaryLabel
-        label.textAlignment = .left
-        label.font = .systemFont(ofSize: 14, weight: .regular)
-        return label
-    }()
-    
-    private let publisherLabel: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 1
-        label.textColor = .secondaryLabel
-        label.textAlignment = .left
-        label.font = .systemFont(ofSize: 14, weight: .regular)
-        return label
-    }()
-    
+//MARK: - Initialization
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setupVC()
+        setup()
+        layout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    private func setupVC() {
+
+//MARK: - Setup
+    private func setup() {
+        iconImageLayer.startPoint = CGPoint(x: 0, y: 0)
+        iconImageLayer.endPoint = CGPoint(x: 1, y: 1)
+        iconImageView.layer.addSublayer(iconImageLayer)
+        
+        titleLayer.startPoint = CGPoint(x: 0, y: 0)
+        titleLayer.endPoint = CGPoint(x: 1, y: 0.5)
+        titleLabel.layer.addSublayer(titleLayer)
+        
+        dateLayer.startPoint = CGPoint(x: 0, y: 0)
+        dateLayer.endPoint = CGPoint(x: 1, y: 0.5)
+        dateLabel.layer.addSublayer(dateLayer)
+        
+        publisherLayer.startPoint = CGPoint(x: 0, y: 0)
+        publisherLayer.endPoint = CGPoint(x: 1, y: 0.5)
+        publisherLabel.layer.addSublayer(publisherLayer)
+        
+        publisherLabel.numberOfLines = 1
+        publisherLabel.textColor = .secondaryLabel
+        publisherLabel.textAlignment = .center
+        publisherLabel.font = .systemFont(ofSize: 12, weight: .regular)
+        
+        dateLabel.numberOfLines = 1
+        dateLabel.textColor = .secondaryLabel
+        dateLabel.textAlignment = .left
+        dateLabel.font = .systemFont(ofSize: 12, weight: .regular)
+
+        titleLabel.numberOfLines = 3
+        titleLabel.textColor = .label
+        titleLabel.textAlignment = .left
+        titleLabel.font = .systemFont(ofSize: 16, weight: .medium)
+        
+        iconImageView.backgroundColor = .secondarySystemBackground
+        iconImageView.contentMode = .scaleAspectFill
+        iconImageView.layer.cornerRadius = 10
+        iconImageView.clipsToBounds = true
+        
+        let imageGroup = makeAnimationGroup()
+        imageGroup.beginTime = 0.0
+        iconImageLayer.add(imageGroup, forKey: "backgroundColor")
+        
+        let titleGroup = makeAnimationGroup(previousGroup: imageGroup)
+        titleLayer.add(titleGroup, forKey: "backgroundColor")
+        
+        let dateGroup = makeAnimationGroup(previousGroup: titleGroup)
+        dateLayer.add(dateGroup, forKey: "backgroundColor")
+        
+        let publisherGroup = makeAnimationGroup(previousGroup: dateGroup)
+        publisherLayer.add(publisherGroup, forKey: "backgroundColor")
+    }
+
+//MARK: - Layout
+    private func layout() {
         contentView.addSubview(iconImageView)
         contentView.addSubview(titleLabel)
         contentView.addSubview(dateLabel)
         contentView.addSubview(publisherLabel)
         
         iconImageView.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview().inset(20)
-            make.top.equalToSuperview().inset(20)
-            make.height.equalTo(200)
+            make.leading.equalToSuperview().inset(20)
+            make.width.height.equalTo(80)
+            make.centerY.equalToSuperview()
         }
         
         titleLabel.snp.makeConstraints { make in
-            make.top.equalTo(iconImageView.snp.bottom).inset(-10)
-            make.leading.trailing.equalTo(iconImageView)
+            make.top.equalTo(iconImageView.snp.top)
+            make.leading.equalTo(iconImageView.snp.trailing).inset(-10)
+            make.trailing.equalToSuperview().inset(20)
         }
         
         dateLabel.snp.makeConstraints { make in
             make.top.equalTo(titleLabel.snp.bottom).inset(-10)
-            make.leading.equalTo(iconImageView.snp.leading)
+            make.leading.equalTo(titleLabel.snp.leading)
             make.width.equalTo(100)
         }
         
         publisherLabel.snp.makeConstraints { make in
             make.top.equalTo(titleLabel.snp.bottom).inset(-10)
             make.leading.equalTo(dateLabel.snp.trailing).inset(-10)
-            make.trailing.equalTo(iconImageView.snp.trailing)
+            make.trailing.equalTo(titleLabel.snp.trailing)
         }
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        iconImageLayer.frame = iconImageView.bounds
+        iconImageLayer.cornerRadius = 5
+        
+        titleLayer.frame = titleLabel.bounds
+        titleLayer.cornerRadius = 3
+        
+        dateLayer.frame = dateLabel.bounds
+        dateLayer.cornerRadius = 3
+        
+        publisherLayer.frame = publisherLabel.bounds
+        publisherLayer.cornerRadius = 3
+    }
+
+//MARK: - Configuration
     func configure(with model: News) {
         guard let url = URL(string: model.urlToImage ?? "") else { return }
         
@@ -100,7 +147,7 @@ final class HeaderNewsTableViewCell: UITableViewCell {
                 .cacheOriginalImage
             ])
         
-        dateLabel.text = model.publishedAt.toDate()?.timeAgoDisplay()
+        dateLabel.text = model.publishedAt.stringToDate()?.timeAgoDisplay()
         titleLabel.text = model.title
         publisherLabel.text = model.source.name
     }
--- R - news/News/View/Cells/HeaderNewsCell.swift
+++ R - news/News/View/Cells/HeaderNewsCell.swift
@@ -5,4 +5,103 @@
 //  Created by anna on 20.08.2022.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Kingfisher
+
+final class HeaderNewsTableViewCell: UITableViewCell {
+
+//MARK: - Identifier
+    static let identifier = "HeaderNewsTableViewCell"
+
+//MARK: - Properties
+    private let iconImageView = UIImageView()
+    private let titleLabel = UILabel()
+    private let dateLabel = UILabel()
+    private let publisherLabel = UILabel()
+
+//MARK: - Initialization
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViewController()
+        setupNewsView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+//MARK: - Setup
+    private func setupViewController() {
+        contentView.addSubview(iconImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(dateLabel)
+        contentView.addSubview(publisherLabel)
+        
+        iconImageView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.top.equalToSuperview().inset(20)
+            make.height.equalTo(200)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(iconImageView.snp.bottom).inset(-10)
+            make.leading.trailing.equalTo(iconImageView)
+        }
+        
+        dateLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).inset(-10)
+            make.leading.equalTo(iconImageView.snp.leading)
+            make.width.equalTo(100)
+        }
+        
+        publisherLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).inset(-10)
+            make.leading.equalTo(dateLabel.snp.trailing).inset(-10)
+            make.trailing.equalTo(iconImageView.snp.trailing)
+        }
+    }
+        
+        private func setupNewsView() {
+            iconImageView.backgroundColor = .secondarySystemBackground
+            iconImageView.contentMode = .scaleAspectFill
+            iconImageView.layer.cornerRadius = 10
+            iconImageView.clipsToBounds = true
+            
+            titleLabel.numberOfLines = 2
+            titleLabel.textColor = .label
+            titleLabel.textAlignment = .left
+            titleLabel.font = .systemFont(ofSize: 18, weight: .medium)
+            
+            dateLabel.numberOfLines = 1
+            dateLabel.textColor = .secondaryLabel
+            dateLabel.textAlignment = .left
+            dateLabel.font = .systemFont(ofSize: 14, weight: .regular)
+            
+            publisherLabel.numberOfLines = 1
+            publisherLabel.textColor = .secondaryLabel
+            publisherLabel.textAlignment = .left
+            publisherLabel.font = .systemFont(ofSize: 14, weight: .regular)
+            
+        }
+
+//MARK: - Configuration
+    func configure(with model: News) {
+        guard let url = URL(string: model.urlToImage ?? "") else { return }
+        
+        iconImageView.kf.setImage(
+            with: url,
+            placeholder: UIImage(named: "placeholderImage"),
+            options: [
+                .scaleFactor(UIScreen.main.scale),
+                .transition(.fade(0.5)),
+                .cacheOriginalImage
+            ])
+        
+        dateLabel.text = model.publishedAt.stringToDate()?.timeAgoDisplay()
+        titleLabel.text = model.title
+        publisherLabel.text = model.source.name
+    }
+}
+
+
